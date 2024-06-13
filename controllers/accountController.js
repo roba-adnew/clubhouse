fe83113a7f4cb217c6289f8dd6a4d7944d49e785
roba <10408936+roba-adnew@users.noreply.github.com>
2024-06-13 15:23:58 -0400

    Enabled password validation at account creation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,42 +1,62 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
 
 exports.accountCreateGet = asyncHandler(async (req, res, next) => {
     console.log('page is loading at least')
     const renderConfig = {
         title: 'Create an account',
-        page: 'signUp'
+        page: 'signUp',
+        passwordsFailMatch: false
     }
     res.render('layout', renderConfig)
 })
 
-exports.accountCreatePost = asyncHandler(async (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        if (err) {
-            console.log("nah, you had issues at the hash")
-            return next(err);
-        }
-        try {
-            const user = new User({
-                username: req.body.username,
-                hashedPassword: hashedPassword,
-                start: Date.now(), // ms
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                status: "new"
-            });
-            const result = await user.save();
-            console.log("successful account creation")
-            res.redirect("/");
-        } catch (err2) {
-            console.log("nah, this time we couldn't make the account")
-            return next(err2);
-        };
-    })
-})
+exports.accountCreatePost = [
+    body('passwordConf').custom((value, req) => {
+        return value === req.body.password
+    }),
+    asyncHandler(async (req, res, next) => {
+        const errors  = validationResult(req);
 
+        if (!errors.isEmpty()) {
+            const renderConfig = {
+                title: 'Create an account',
+                page: 'signUp',
+                passwordsFailMatch: true
+            }
+
+            console.log('dont match');
+            res.render('layout', renderConfig);
+            return
+        }
+
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                console.log("nah, you had issues at the hash")
+                return next(err);
+            }
+            try {
+                const user = new User({
+                    username: req.body.username,
+                    hashedPassword: hashedPassword,
+                    start: Date.now(), // ms
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    status: "new"
+                });
+                const result = await user.save();
+                console.log("successful account creation")
+                res.redirect("/");
+            } catch (err2) {
+                console.log("nah, this time we couldn't make the account")
+                return next(err2);
+            };
+        })
+    })
+]
 exports.loginGet = asyncHandler(async (req, res, next) => {
     const renderConfig = {
         page: 'login',
@@ -48,4 +68,4 @@ exports.loginGet = asyncHandler(async (req, res, next) => {
 exports.loginPost = passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login"
-  })
+})
