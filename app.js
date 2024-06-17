dfe7f7c5396126eb75aa0f67d984d077133a8624
roba <10408936+roba-adnew@users.noreply.github.com>
2024-06-17 14:58:13 -0400

    Improved username and password validation on account creation

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ main();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.session());
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- controllers/accountController.js
+++ controllers/accountController.js
@@ -10,30 +10,46 @@ exports.accountCreateGet = asyncHandler(async (req, res, next) => {
         title: 'Create an account',
         page: 'signUp',
         passwordsFailMatch: false,
+        userExists: false,
         user: res.locals.currentUser
     }
     res.render('layout', renderConfig)
 })
 
 exports.accountCreatePost = [
-    body('passwordConf').custom((value, req) => {
+    body('passwordConf').custom((value,{ req }) => {
         return value === req.body.password
     }),
     asyncHandler(async (req, res, next) => {
-        const errors  = validationResult(req);
+        const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
             const renderConfig = {
                 title: 'Create an account',
                 page: 'signUp',
                 passwordsFailMatch: true,
+                userExists: false,
                 user: res.locals.currentUser
             }
 
-            console.log('dont match');
+            console.log('issue at password confirmation');
             res.render('layout', renderConfig);
             return
         }
+
+        const user = await User.findOne({ username: req.body.username });
+        if (user) {
+            const renderConfig = {
+                title: 'Create an account',
+                page: 'signUp',
+                passwordsFailMatch: true,
+                userExists: true,
+                user: res.locals.currentUser,
+            }
+            console.log('user already exists');
+            res.render('layout', renderConfig);
+            return
+        };
 
         bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
             if (err) {
@@ -49,7 +65,7 @@ exports.accountCreatePost = [
                     lastName: req.body.lastName,
                     status: "uninitiated"
                 });
-                const result = await user.save  ();
+                const result = await user.save();
                 console.log("successful account creation")
                 res.redirect("/");
             } catch (err2) {
@@ -59,6 +75,7 @@ exports.accountCreatePost = [
         })
     })
 ]
+
 exports.loginGet = asyncHandler(async (req, res, next) => {
     const renderConfig = {
         page: 'login',
@@ -108,7 +125,7 @@ exports.passcodePost = [
         return value === 'chinchilla'
     }),
     asyncHandler(async (req, res) => {
-        const errors  = validationResult(req);
+        const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
             const renderConfig = {
