f413ee74e82ef25efff8b264449c65209f1fd742
roba <10408936+roba-adnew@users.noreply.github.com>
2024-06-26 14:41:32 -0400

    removing a number of files added to gitignore

--- routes/auth.js
+++ routes/auth.js
@@ -12,38 +12,6 @@ router.post('/sign-up', accountController.accountCreatePost)
 
 router.get('/login', accountController.loginGet);
 
-// passport set-up
-passport.use(
-    new LocalStrategy(async (username, password, done) => {
-        try {
-            const user = await User.findOne({ username: username });
-            if (!user) {
-                return done(null, false, { message: "Incorrect username" });
-            };
-            const match = await bcrypt.compare(password, user.hashedPassword);
-            if (!match) {
-                return done(null, false, { message: "Incorrect password" });
-            };
-            return done(null, user);
-        } catch (err) {
-            return done(err);
-        };
-    })
-);
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (err) {
-        done(err);
-    };
-});
-
 router.use((req, res, next) => {
     res.locals.currentUser = req.user;
     next();
